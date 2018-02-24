1900f15dd793a568913031b3a21d5d269b146002
Stefanos Stefanou <stefan1998xd@gmail.com>
2018-02-24 05:38:03 +0200

    Quick and dirty .dot animation , refactor comes soooon!

--- app/src/main/java/com/example/stefstef/criminalintent/CrimePickerPagerDialog.java
+++ app/src/main/java/com/example/stefstef/criminalintent/CrimePickerPagerDialog.java
@@ -14,6 +14,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 
 import java.util.Date;
 
@@ -22,7 +23,10 @@ import java.util.Date;
  */
 
 public class CrimePickerPagerDialog extends DialogFragment {
-    private ViewPager pager;
+    private ViewPager   pager;
+    private View        v;
+    private RadioButton ch1;
+    private RadioButton ch2;
 
     /**
      * Perform initialization of all fragments and loaders.
@@ -37,16 +41,25 @@ public class CrimePickerPagerDialog extends DialogFragment {
     @NonNull
     @Override
     public View onCreateView(LayoutInflater infl, ViewGroup viewGroup,Bundle save){
-        this.pager=new ViewPager(this.getActivity());
+        this.v = infl.inflate(R.layout.datetime_config,viewGroup);
+        this.pager=v.findViewById(R.id.viewPager);
+        this.ch1=v.findViewById(R.id.ch1);
+        this.ch2=v.findViewById(R.id.ch2);
+        //this.pager=new ViewPager(this.getActivity());
         this.pager.setId(R.id.pickerViewPager);
         final FragmentManager fm = this.getChildFragmentManager();
         this.pager.setAdapter(new FragmentStatePagerAdapter(fm) {
             @Override
             public Fragment getItem(int position) {
                 if(position==0){
+                    CrimePickerPagerDialog.this.ch1.setChecked(true);
+                    CrimePickerPagerDialog.this.ch2.setChecked(false);
                     return CrimeDatePicker.getInstance(new Date(),null,CrimeFragment.REQUEST_NEW_DATE   );
 
                 }
+
+                CrimePickerPagerDialog.this.ch1.setChecked(false);
+                CrimePickerPagerDialog.this.ch2.setChecked(true);
                 return CrimeTimePicker.getInstance(new Date(),null,12);
             }
 
@@ -55,6 +68,32 @@ public class CrimePickerPagerDialog extends DialogFragment {
                 return 2;
             }
         });
-        return this.pager;
+        this.pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position==0){
+                    CrimePickerPagerDialog.this.ch1.setChecked(true);
+                    CrimePickerPagerDialog.this.ch2.setChecked(false);
+                    return;
+                    //return CrimeDatePicker.getInstance(new Date(),null,CrimeFragment.REQUEST_NEW_DATE   );
+
+                }
+
+                CrimePickerPagerDialog.this.ch1.setChecked(false);
+                CrimePickerPagerDialog.this.ch2.setChecked(true);
+                //return CrimeTimePicker.getInstance(new Date(),null,12);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        return v;
     }
 }
